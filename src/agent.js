15ea8944a75712b45206d3a2a306d674e591576f
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-01-26 16:19:57 +0330

    switched to golang backend

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,8 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://conduit.productionready.io/api';
+//const API_ROOT = 'https://conduit.productionready.io/api';
+const API_ROOT = 'http://localhost:8585/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
