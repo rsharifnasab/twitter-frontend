e1de7554a17d558c17cf7d28bc10718d6aca1fd3
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-01-26 22:54:36 +0330

    solved multi line tweet problem

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,100 +2,116 @@ import ArticleMeta from './ArticleMeta';
 import CommentContainer from './CommentContainer';
 import React from 'react';
 import agent from '../../agent';
-import { connect } from 'react-redux';
+import {
+    connect
+} from 'react-redux';
 import marked from 'marked';
-import { ARTICLE_PAGE_LOADED, ARTICLE_PAGE_UNLOADED } from '../../constants/actionTypes';
+import {
+    ARTICLE_PAGE_LOADED,
+    ARTICLE_PAGE_UNLOADED
+} from '../../constants/actionTypes';
 
 const mapStateToProps = state => ({
-  ...state.article,
-  currentUser: state.common.currentUser
+    ...state.article,
+    currentUser: state.common.currentUser
 });
 
 const mapDispatchToProps = dispatch => ({
-  onLoad: payload =>
-    dispatch({ type: ARTICLE_PAGE_LOADED, payload }),
-  onUnload: () =>
-    dispatch({ type: ARTICLE_PAGE_UNLOADED })
+    onLoad: payload =>
+    dispatch({
+        type: ARTICLE_PAGE_LOADED,
+        payload
+    }),
+    onUnload: () =>
+    dispatch({
+        type: ARTICLE_PAGE_UNLOADED
+    })
 });
 
 class Article extends React.Component {
-  componentWillMount() {
-    this.props.onLoad(Promise.all([
-      agent.Articles.get(this.props.match.params.id),
-      agent.Comments.forArticle(this.props.match.params.id)
-    ]));
-  }
-
-  componentWillUnmount() {
-    this.props.onUnload();
-  }
-
-  render() {
-    if (!this.props.article) {
-      return null;
+    componentWillMount() {
+        this.props.onLoad(Promise.all([
+            agent.Articles.get(this.props.match.params.id),
+            agent.Comments.forArticle(this.props.match.params.id)
+        ]));
     }
 
-    const markup = { __html: marked(this.props.article.body, { sanitize: true }) };
-    const canModify = this.props.currentUser &&
-      this.props.currentUser.username === this.props.article.author.username;
-    return (
-      <div className="article-page">
+    componentWillUnmount() {
+        this.props.onUnload();
+    }
 
-        <div className="banner">
-          <div className="container">
+    render() {
+        if (!this.props.article) {
+            return null;
+        }
 
-            <h1>{this.props.article.title}</h1>
-            <ArticleMeta
-              article={this.props.article}
-              canModify={canModify} />
+        const markup = {
+            __html: marked(this.props.article.body, {
+                sanitize: true
+            })
+        };
+        const canModify = this.props.currentUser &&
+            this.props.currentUser.username === this.props.article.author.username;
+        return (
+            <div className="article-page">
 
-          </div>
-        </div>
+                <div className="banner">
+                    <div className="container">
 
-        <div className="container page">
+                        <h1>{this.props.article.title}</h1>
+                        <ArticleMeta
+                            article={this.props.article}
+                            canModify={canModify} />
 
-          <div className="row article-content">
-            <div className="col-xs-12">
-
-                {/* <div dangerouslySetInnerHTML={markup}></div> */}
-                <div>
-                    {this.props.article.body}
+                    </div>
                 </div>
 
+                <div className="container page">
 
-              <ul className="tag-list">
-                {
-                  this.props.article.tagList.map(tag => {
-                    return (
-                      <li
-                        className="tag-default tag-pill tag-outline"
-                        key={tag}>
-                        {tag}
-                      </li>
-                    );
-                  })
-                }
-              </ul>
+                    <div className="row article-content">
+                        <div className="col-xs-12">
 
+                            {/* <div dangerouslySetInnerHTML={markup}></div> */}
+                            <div>
+                                <pre>
+                                    {this.props.article.body}
+                                </pre>
+                            </div>
+
+
+                            <ul className="tag-list">
+                                {
+                                    this.props.article.tagList.map(tag => {
+                                        return (
+                                            <li
+                                                className="tag-default tag-pill tag-outline"
+                                                key={tag}>
+                                                {tag}
+                                            </li>
+                                        );
+                                    })
+                                }
+                            </ul>
+
+                        </div>
+                    </div>
+
+                    <hr />
+
+                    <div className="article-actions">
+                    </div>
+
+                    <div className="row">
+                        <CommentContainer
+                            comments={this.props.comments || []}
+                            errors={this.props.commentErrors}
+                            slug={this.props.match.params.id}
+                            currentUser={this.props.currentUser} />
+                    </div>
+                </div>
             </div>
-          </div>
-
-          <hr />
-
-          <div className="article-actions">
-          </div>
-
-          <div className="row">
-            <CommentContainer
-              comments={this.props.comments || []}
-              errors={this.props.commentErrors}
-              slug={this.props.match.params.id}
-              currentUser={this.props.currentUser} />
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Article);
