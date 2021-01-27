3a11b707990f3e6e1fa113acffe94014d92d40cf
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-01-28 00:32:13 +0330

    commented unused markup

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -5,7 +5,7 @@ import agent from '../../agent';
 import {
     connect
 } from 'react-redux';
-import marked from 'marked';
+//import marked from 'marked';
 import {
     ARTICLE_PAGE_LOADED,
     ARTICLE_PAGE_UNLOADED
@@ -45,11 +45,13 @@ class Article extends React.Component {
             return null;
         }
 
+        /*
         const markup = {
             __html: marked(this.props.article.body, {
                 sanitize: true
             })
         };
+        */
         const canModify = this.props.currentUser &&
             this.props.currentUser.username === this.props.article.author.username;
         return (
